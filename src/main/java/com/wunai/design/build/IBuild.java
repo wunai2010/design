49b3cca62a6550b0eb03579e6a31f16591966bfa
wangyuebai <wangyuebai@zhongan.io>
2020-03-01 21:08:15 +0800

    生成器模式

---
+++ src/main/java/com/wunai/design/build/IBuild.java
@@ -0,0 +1,9 @@
+package com.wunai.design.build;
+
+public interface IBuild {
+     public void createUnit1();
+     public void createUnit2();
+     public void createUnit3();
+     public Product composite();
+
+}
